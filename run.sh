7a02487f96a1aedf55e4a81f11b5f7ed1a270c0e
ControlNet <smczx@hotmail.com>
2023-03-13 20:35:13 +1100

    update

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-DEVELOPMENT=false ENABLE_FLAC=true ENABLE_LOCAL_VIP=true SIGN_KEY="./server.key" SIGN_CERT="./server.crt" node ./app.js -p 7890:7891
+DEVELOPMENT=false ENABLE_FLAC=true ENABLE_LOCAL_VIP=true NO_CACHE=false SIGN_KEY="./server.key" SIGN_CERT="./server.crt" node ./app.js -p 7890:7891
 
---
+++ run_pm2.sh
@@ -0,0 +1,1 @@
+pm2 start run.sh --cron-restart="0/5 * * * *" --name UnblockNeteaseMusic
